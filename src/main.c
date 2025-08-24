cf71961110f11b6bdb7549d0b1e424dc15f8e239
Abel_personal <abel.aguilarc8@gmail.com>
2025-08-24 18:20:59 +0100

    Modify: Main function to take in the number of disks in the Hanoi tower

--- src/main.c
+++ src/main.c
@@ -1,32 +1,29 @@
 #include <stdio.h> 
-#include "stack.h"
+#include "Hanoi.h"
 
 int main(int argv, char** argc) {
-  Stack_t stack = stack_create(10);
-
-  stack_push(&stack, 4);
-  stack_push(&stack, 8);
-  stack_push(&stack, 15);
-  stack_push(&stack, 16);
-  stack_push(&stack, 23);
-
-  uint8_t popped_value;
-  if (stack_pop(&stack, &popped_value) == STACK_OK) {
-    printf("Popped value: %d\n", popped_value);
-  } else {
-    printf("Error popping the stack\n");
-  }
+  uint8_t num_disks = 3; // Default number of disks
   
-  printf("Peek the top value: ");
-  uint8_t top_value;
-  if (stack_peek(&stack, &top_value) == STACK_OK) {
-    printf("%d\n", top_value);
-  } else {
-    printf("Error peeking the stack\n");
+  if (argv > 1) {
+    int tmp = atoi(argc[1]);
+    if (tmp > 0 && tmp <= UINT8_MAX) {
+      num_disks = (uint8_t)tmp;
+    } else {
+      printf("Invalid number of disks, using default of 3\n\n");
+    }
   }
 
-  stack_print(&stack);
-  stack_destroy(&stack);
+  Hanoi_t hanoi = hanoi_init(num_disks);
+
+  Status_t status = hanoi_solve(&hanoi);
+
+  // printf("\n-------------------------\n");
+  // hanoi_print(&hanoi);
+  if (status != HANOI_OK) {
+    printf("Error solving Tower of Hanoi: %d\n", status);
+  }
+
+  hanoi_destroy(&hanoi);
 
   return 0;
 }
